d566bc2d71e61e6441e2889f1adec893018e4627
gerruh <avvewyccstrila@gmail.com>
2026-06-14 14:28:45 +0300

    added User model, added django-ninja-jwt dependency

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ load_dotenv(dotenv_path=env_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -31,7 +30,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -44,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.doctor',
     'apps.facility',
-    'apps.procedure'
+    'apps.procedure',
+    'apps.user'
 ]
 
 MIDDLEWARE = [
@@ -79,7 +78,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 PAGINATION_PER_PAGE = int(os.environ.get('PAGINATION_PER_PAGE', 10))
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -94,9 +92,9 @@ DATABASES = {
     }
 }
 
+# AUTH
 
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'user.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -124,7 +121,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
